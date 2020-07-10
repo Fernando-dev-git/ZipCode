55ea869b058df7e20d0ecc16741706e4697a1691
Fernando-dev-git <isc.foctaviosanchez@gmail.com>
2020-07-10 18:44:59 -0500

    servicio de codigo postal

---
+++ src/main/java/com/maverick/codigo/postal/SpringbootServicioCodigoPostalApplication.java
@@ -0,0 +1,13 @@
+package com.maverick.codigo.postal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioCodigoPostalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioCodigoPostalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maverick/codigo/postal/SpringbootServicioCodigoPostalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maverick.codigo.postal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioCodigoPostalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
